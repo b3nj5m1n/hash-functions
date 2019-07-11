c5bbcb498c3148e5ef37093ab90dad74cb734760
b3nj5m1n <47924309+b3nj5m1n@users.noreply.github.com>
2019-07-11 17:53:28 +0200

    Added sha512

--- sha.py
+++ sha.py
@@ -53,6 +53,11 @@ class sched:
         if t >= 16:
             return int((sched.sigma1_sha384(init_words[t-2]) + init_words[t-7] + sched.sigma0_sha384(init_words[t-15]) + init_words[t-16])   % 2 ** 64)
         return init_words[t]
+    @staticmethod
+    def MIX_512(t, init_words, w=64):
+        if t >= 16:
+            return int((sched.sigma1_sha384(init_words[t-2]) + init_words[t-7] + sched.sigma0_sha384(init_words[t-15]) + init_words[t-16])   % 2 ** 64)
+        return init_words[t]
 
     # FIPS-180-4 6.1.2
     # Create message schedule for block i
@@ -79,6 +84,14 @@ class sched:
             W.append(sched.MIX_sha384(t, inital_words))
         for t in range(16, 80):
             W.append(sched.MIX_sha384(t, W))
+        return W
+    @staticmethod
+    def create_schedule_sha512(inital_words):
+        W = []
+        for t in range(0, 16):
+            W.append(sched.MIX_512(t, inital_words))
+        for t in range(16, 80):
+            W.append(sched.MIX_512(t, W))
         return W
 
 class ppp:
@@ -500,6 +513,99 @@ class hash:
         int('db0c2e0d64f98fa7', 16),
         int('47b5481dbefa4fa4', 16)]
 
+    # FIPS-180-4 4.2.3
+    # Constant values
+    K_sha512 = [int('428a2f98d728ae22', 16),
+        int('7137449123ef65cd', 16),
+        int('b5c0fbcfec4d3b2f', 16),
+        int('e9b5dba58189dbbc', 16),
+        int('3956c25bf348b538', 16),
+        int('59f111f1b605d019', 16),
+        int('923f82a4af194f9b', 16),
+        int('ab1c5ed5da6d8118', 16),
+        int('d807aa98a3030242', 16),
+        int('12835b0145706fbe', 16),
+        int('243185be4ee4b28c', 16),
+        int('550c7dc3d5ffb4e2', 16),
+        int('72be5d74f27b896f', 16),
+        int('80deb1fe3b1696b1', 16),
+        int('9bdc06a725c71235', 16),
+        int('c19bf174cf692694', 16),
+        int('e49b69c19ef14ad2', 16),
+        int('efbe4786384f25e3', 16),
+        int('0fc19dc68b8cd5b5', 16),
+        int('240ca1cc77ac9c65', 16),
+        int('2de92c6f592b0275', 16),
+        int('4a7484aa6ea6e483', 16),
+        int('5cb0a9dcbd41fbd4', 16),
+        int('76f988da831153b5', 16),
+        int('983e5152ee66dfab', 16),
+        int('a831c66d2db43210', 16),
+        int('b00327c898fb213f', 16),
+        int('bf597fc7beef0ee4', 16),
+        int('c6e00bf33da88fc2', 16),
+        int('d5a79147930aa725', 16),
+        int('06ca6351e003826f', 16),
+        int('142929670a0e6e70', 16),
+        int('27b70a8546d22ffc', 16),
+        int('2e1b21385c26c926', 16),
+        int('4d2c6dfc5ac42aed', 16),
+        int('53380d139d95b3df', 16),
+        int('650a73548baf63de', 16),
+        int('766a0abb3c77b2a8', 16),
+        int('81c2c92e47edaee6', 16),
+        int('92722c851482353b', 16),
+        int('a2bfe8a14cf10364', 16),
+        int('a81a664bbc423001', 16),
+        int('c24b8b70d0f89791', 16),
+        int('c76c51a30654be30', 16),
+        int('d192e819d6ef5218', 16),
+        int('d69906245565a910', 16),
+        int('f40e35855771202a', 16),
+        int('106aa07032bbd1b8', 16),
+        int('19a4c116b8d2d0c8', 16),
+        int('1e376c085141ab53', 16),
+        int('2748774cdf8eeb99', 16),
+        int('34b0bcb5e19b48a8', 16),
+        int('391c0cb3c5c95a63', 16),
+        int('4ed8aa4ae3418acb', 16),
+        int('5b9cca4f7763e373', 16),
+        int('682e6ff3d6b2b8a3', 16),
+        int('748f82ee5defb2fc', 16),
+        int('78a5636f43172f60', 16),
+        int('84c87814a1f0ab72', 16),
+        int('8cc702081a6439ec', 16),
+        int('90befffa23631e28', 16),
+        int('a4506cebde82bde9', 16),
+        int('bef9a3f7b2c67915', 16),
+        int('c67178f2e372532b', 16),
+        int('ca273eceea26619c', 16),
+        int('d186b8c721c0c207', 16),
+        int('eada7dd6cde0eb1e', 16),
+        int('f57d4f7fee6ed178', 16),
+        int('06f067aa72176fba', 16),
+        int('0a637dc5a2c898a6', 16),
+        int('113f9804bef90dae', 16),
+        int('1b710b35131c471b', 16),
+        int('28db77f523047d84', 16),
+        int('32caab7b40c72493', 16),
+        int('3c9ebe0a15c9bebc', 16),
+        int('431d67c49c100d4c', 16),
+        int('4cc5d4becb3e42b6', 16),
+        int('597f299cfc657e2a', 16),
+        int('5fcb6fab3ad6faec', 16),
+        int('6c44198c4a475817', 16)]
+    # FIPS-180-4 5.3.3
+    # Constant inital hash values
+    H_sha512 = [int('6a09e667f3bcc908', 16),
+        int('bb67ae8584caa73b', 16),
+        int('3c6ef372fe94f82b', 16),
+        int('a54ff53a5f1d36f1', 16),
+        int('510e527fade682d1', 16),
+        int('9b05688c2b3e6c1f', 16),
+        int('1f83d9abfb41bd6b', 16),
+        int('5be0cd19137e2179', 16)]
+
 
     # Main function, return sha160 hash of input
     # Message_format's:
@@ -645,6 +751,41 @@ class hash:
             # Add word in hex to $msg string variable
             msg += hex(w)[2:].zfill(8)
         return msg
+    @staticmethod
+    def sha512(message, message_format=0):
+        # Set inital message
+        inital_message = ppp.from_str(message)
+        # Convert message if neccessary
+        if message_format == 0:
+            inital_message = ppp.from_str(message)
+        elif message_format == 1:
+            inital_message = ppp.from_int(message)
+        elif message_format == 2:
+            inital_message = ppp.from_hex(message)
+        elif message_format == 3:
+            inital_message = ppp.from_bin(message)
+        elif message_format == 4:
+            inital_message = ppp.from_oct(message)
+        elif message_format == 5:
+            inital_message = ppp.from_file(message)
+        # Preproccess (converted) message (Padding & Parsing)
+        preproccessed_message = prep_sha384.prep(inital_message)
+        # Set H variable with inital hash value
+        H = [hash.get_H_sha512()]
+        # FIPS-180-4 6.2.2
+        # Foreach parsed block, create message schedule and hash, then append hash values to $H
+        for i in range(1, len(preproccessed_message) + 1):
+            schedule = sched.create_schedule_sha512(preproccessed_message[i-1])
+            message_hashed = hash.hash_sha512(schedule, H, i)
+            H.append(message_hashed)
+        # Create msg variable (This will be final result)
+        msg = ''
+        # Foreach word in the last entry of H
+        for w in H[-1]:
+            # Add word in hex to $msg string variable
+            msg += hex(w)[2:].zfill(8)
+        return msg
+
 
     # FIPS-180-4 2.2.2
     # Rotate bits to the right
@@ -719,6 +860,9 @@ class hash:
     @staticmethod
     def get_H_sha384():
         return hash.H_sha384
+    @staticmethod
+    def get_H_sha512():
+        return hash.H_sha512
     
     # FIPS-180-4 6.2.2
     @staticmethod
@@ -824,5 +968,42 @@ class hash:
         H5 = (H[i-1][5] + f) % 2 ** 64
         # Return final hash values
         return [H0, H1, H2, H3, H4, H5]
+    @staticmethod
+    def hash_sha512(W, H, i):
+        # Set inital hash values from previous (final) hash values
+        a = H[i-1][0]
+        b = H[i-1][1]
+        c = H[i-1][2]
+        d = H[i-1][3]
+        e = H[i-1][4]
+        f = H[i-1][5]
+        g = H[i-1][6]
+        h = H[i-1][7]
+        # Iterate 80 times
+        for t in range(0, 80):
+            # Calculate temporary value 1
+            T1 = int((h + hash.SIGMA1_sha384(e) + hash.Ch(e, f, g) + hash.K_sha512[t] + W[t]) % 2 ** 64)
+            # Calculate temporary value 2
+            T2 = int((hash.SIGMA0_sha384(a) + hash.Maj(a, b, c)) % 2 ** 64)
+            h = g
+            g = f
+            f = e
+            e = int((d + T1)  % 2 ** 64)
+            d = c
+            c = b
+            b = a
+            a = (T1 + T2) % 2 ** 64
+        # Calculate final hash values
+        H0 = (H[i-1][0] + a) % 2 ** 64
+        H1 = (H[i-1][1] + b) % 2 ** 64
+        H2 = (H[i-1][2] + c) % 2 ** 64
+        H3 = (H[i-1][3] + d) % 2 ** 64
+        H4 = (H[i-1][4] + e) % 2 ** 64
+        H5 = (H[i-1][5] + f) % 2 ** 64
+        H6 = (H[i-1][6] + g) % 2 ** 64
+        H7 = (H[i-1][7] + h) % 2 ** 64
+        # Return final hash values
+        return [H0, H1, H2, H3, H4, H5, H6, H7]
+
 
 
