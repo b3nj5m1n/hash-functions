de2299d3bcf1d06e37972ef1d8ca09ad8c0956ba
b3nj5m1n <47924309+b3nj5m1n@users.noreply.github.com>
2019-07-11 17:14:48 +0200

    Further generalised sha160; Started on sha224

--- sha.py
+++ sha.py
@@ -145,7 +145,7 @@ class hash:
 
     # FIPS-180-4 4.2.2
     # Constant values
-    K = []
+    K_sha160 = []
     # FIPS-180-4 5.3.1
     # Constant inital hash values
     H_sha160 = [int('67452301', 16),
@@ -153,6 +153,82 @@ class hash:
         int('98badcfe', 16),
         int('10325476', 16),
         int('c3d2e1f0', 16)]
+
+    K_sha224 = [int('428a2f98', 16),
+        int('71374491', 16),
+        int('b5c0fbcf', 16),
+        int('e9b5dba5', 16),
+        int('3956c25b', 16),
+        int('59f111f1', 16),
+        int('923f82a4', 16),
+        int('ab1c5ed5', 16),
+        int('d807aa98', 16),
+        int('12835b01', 16),
+        int('243185be', 16),
+        int('550c7dc3', 16),
+        int('72be5d74', 16),
+        int('80deb1fe', 16),
+        int('9bdc06a7', 16),
+        int('c19bf174', 16),
+        int('e49b69c1', 16),
+        int('efbe4786', 16),
+        int('0fc19dc6', 16),
+        int('240ca1cc', 16),
+        int('2de92c6f', 16),
+        int('4a7484aa', 16),
+        int('5cb0a9dc', 16),
+        int('76f988da', 16),
+        int('983e5152', 16),
+        int('a831c66d', 16),
+        int('b00327c8', 16),
+        int('bf597fc7', 16),
+        int('c6e00bf3', 16),
+        int('d5a79147', 16),
+        int('06ca6351', 16),
+        int('14292967', 16),
+        int('27b70a85', 16),
+        int('2e1b2138', 16),
+        int('4d2c6dfc', 16),
+        int('53380d13', 16),
+        int('650a7354', 16),
+        int('766a0abb', 16),
+        int('81c2c92e', 16),
+        int('92722c85', 16),
+        int('a2bfe8a1', 16),
+        int('a81a664b', 16),
+        int('c24b8b70', 16),
+        int('c76c51a3', 16),
+        int('d192e819', 16),
+        int('d6990624', 16),
+        int('f40e3585', 16),
+        int('106aa070', 16),
+        int('19a4c116', 16),
+        int('1e376c08', 16),
+        int('2748774c', 16),
+        int('34b0bcb5', 16),
+        int('391c0cb3', 16),
+        int('4ed8aa4a', 16),
+        int('5b9cca4f', 16),
+        int('682e6ff3', 16),
+        int('748f82ee', 16),
+        int('78a5636f', 16),
+        int('84c87814', 16),
+        int('8cc70208', 16),
+        int('90befffa', 16),
+        int('a4506ceb', 16),
+        int('bef9a3f7', 16),
+        int('c67178f2', 16)
+        ]
+    # FIPS-180-4 5.3.2
+    # Constant inital hash values
+    H_sha224 = [int('c1059ed8', 16),
+        int('367cd507', 16),
+        int('3070dd17', 16),
+        int('f70e5939', 16),
+        int('ffc00b31', 16),
+        int('68581511', 16),
+        int('64f98fa7', 16),
+        int('befa4fa4', 16)]
 
     # Main function, return sha160 hash of input
     # Message_format's:
@@ -187,12 +263,46 @@ class hash:
         # Foreach parsed block, create message schedule and hash, then append hash values to $H_sha160
         for i in range(1, len(preproccessed_message) + 1):
             schedule = sched.create_schedule(preproccessed_message[i-1])
-            message_hashed = hash.hash(schedule, H_sha160, i)
+            message_hashed = hash.hash_sha160(schedule, H_sha160, i)
             H_sha160.append(message_hashed)
         # Create msg variable (This will be final result)
         msg = ''
         # Foreach word in the last entry of H_sha160
         for w in H_sha160[-1]:
+            # Add word in hex to $msg string variable
+            msg += hex(w)[2:].zfill(8)
+        return msg
+    @staticmethod
+    def sha224(message, message_format=0):
+        # Set inital message
+        inital_message = ppp.from_str(message)
+        # Convert message if neccessary
+        if message_format == 0:
+            inital_message = ppp.from_str(message)
+        elif message_format == 1:
+            inital_message = ppp.from_int(message)
+        elif message_format == 2:
+            inital_message = ppp.from_hex(message)
+        elif message_format == 3:
+            inital_message = ppp.from_bin(message)
+        elif message_format == 4:
+            inital_message = ppp.from_oct(message)
+        elif message_format == 5:
+            inital_message = ppp.from_file(message)
+        # Preproccess (converted) message (Padding & Parsing)
+        preproccessed_message = prep.prep(inital_message)
+        # Set H variable with inital hash value
+        H = [hash.get_H_sha224()]
+        # FIPS-180-4 6.2.2
+        # Foreach parsed block, create message schedule and hash, then append hash values to $H
+        for i in range(1, len(preproccessed_message) + 1):
+            schedule = sched.create_schedule(preproccessed_message[i-1])
+            message_hashed = hash.hash_sha224(schedule, H, i)
+            H.append(message_hashed)
+        # Create msg variable (This will be final result)
+        msg = ''
+        # Foreach word in the last entry of H
+        for w in H[-1][:7]:
             # Add word in hex to $msg string variable
             msg += hex(w)[2:].zfill(8)
         return msg
@@ -236,7 +346,7 @@ class hash:
 
     # FIPS-180-4 4.1.1
     @staticmethod
-    def f(x, y, z, t):
+    def f_sha160(x, y, z, t):
         if 0 <= t and t <= 19:
             return hash.Ch(x, y, z)
         if 20 <= t and t <= 39:
@@ -245,16 +355,29 @@ class hash:
             return hash.Maj(x, y, z)
         if 60 <= t and t <= 79:
             return hash.Parity(x, y, z)
+    @staticmethod
+    def f_sha224(x, y, z, t):
+        if 0 <= t and t <= 19:
+            return hash_sha224.Ch(x, y, z)
+        if 20 <= t and t <= 39:
+            return hash_sha224.Parity(x, y, z)
+        if 40 <= t and t <= 59:
+            return hash_sha224.Maj(x, y, z)
+        if 60 <= t and t <= 79:
+            return hash_sha224.Parity(x, y, z)
 
     # FIPS-180-4 5.3.2
     # Get the (constant) inital hash values
     @staticmethod
     def get_H_sha160():
         return hash.H_sha160
+    @staticmethod
+    def get_H_sha224():
+        return hash.H_sha224
     
     # FIPS-180-4 6.2.2
     @staticmethod
-    def hash(W, H_sha160, i):
+    def hash_sha160(W, H_sha160, i):
         # Set inital hash values from previous (final) hash values
         a = H_sha160[i-1][0]
         b = H_sha160[i-1][1]
@@ -264,15 +387,15 @@ class hash:
         # Iterate 80 times
         for t in range(80):
             if 0 <= t <= 19:
-                hash.K.append(int('5A827999', 16))
+                hash.K_sha160.append(int('5A827999', 16))
             elif 20 <= t <= 39:
-                hash.K.append(int('6ED9EBA1', 16))
+                hash.K_sha160.append(int('6ED9EBA1', 16))
             elif 40 <= t <= 59:
-                hash.K.append(int('8F1BBCDC', 16))
+                hash.K_sha160.append(int('8F1BBCDC', 16))
             elif 60 <= t <= 79:
-                hash.K.append(int('CA62C1D6', 16))
+                hash.K_sha160.append(int('CA62C1D6', 16))
             # Calculate temporary value
-            T = int((hash.ROTL(a, 5) + hash.f(b, c, d, t) + e + hash.K[t] + W[t]) % 2 ** 32)
+            T = int((hash.ROTL(a, 5) + hash.f_sha160(b, c, d, t) + e + hash.K_sha160[t] + W[t]) % 2 ** 32)
             e = d
             d = c
             c = hash.ROTL(b, 30)
@@ -286,4 +409,39 @@ class hash:
         H4 = (H_sha160[i-1][4] + e) % 2 ** 32
         # Return final hash values
         return [H0, H1, H2, H3, H4]
-
+    @staticmethod
+    def hash_sha224(W, H, i):
+        # Set inital hash values from previous (final) hash values
+        a = H[i-1][0]
+        b = H[i-1][1]
+        c = H[i-1][2]
+        d = H[i-1][3]
+        e = H[i-1][4]
+        f = H[i-1][5]
+        g = H[i-1][6]
+        h = H[i-1][7]
+        # Iterate 64 times
+        for t in range(0, 64):
+            # Calculate temporary value 1
+            T1 = int((h + hash.SIGMA1(e) + hash.Ch(e, f, g) + hash.K_sha224[t] + W[t]) % 2 ** 32)
+            # Calculate temporary value 2
+            T2 = int((hash.SIGMA0(a) + hash.Maj(a, b, c)) % 2 ** 32)
+            h = g
+            g = f
+            f = e
+            e = int((d + T1)  % 2 ** 32)
+            d = c
+            c = b
+            b = a
+            a = (T1 + T2) % 2 ** 32
+        # Calculate final hash values
+        H0 = (H[i-1][0] + a) % 2 ** 32
+        H1 = (H[i-1][1] + b) % 2 ** 32
+        H2 = (H[i-1][2] + c) % 2 ** 32
+        H3 = (H[i-1][3] + d) % 2 ** 32
+        H4 = (H[i-1][4] + e) % 2 ** 32
+        H5 = (H[i-1][5] + f) % 2 ** 32
+        H6 = (H[i-1][6] + g) % 2 ** 32
+        H7 = (H[i-1][7] + h) % 2 ** 32
+        # Return final hash values
+        return [H0, H1, H2, H3, H4, H5, H6, H7]
