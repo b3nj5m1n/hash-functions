586da869171afd836a7ca067a536289e4917165b
b3nj5m1n <47924309+b3nj5m1n@users.noreply.github.com>
2019-07-11 18:21:49 +0200

    Added display for all functions

--- main.py
+++ main.py
@@ -8,6 +8,12 @@ import sha
 # Main function / Entry point | Set args variable to given arguments, except for the first one because that is just the script name (main.py, in this case)
 def main(args=sys.argv[1:]):
     # Print hash value of the first argument, interpret first argument as $mode
-    print(hash.sha512_t(args[0], 256))
+    print("sha160:\n" + str(hash.sha160(args[0], args[1])))
+    print("sha224:\n" + str(hash.sha224(args[0], args[1])))
+    print("sha256:\n" + str(hash.sha256(args[0], args[1])))
+    print("sha384:\n" + str(hash.sha384(args[0], args[1])))
+    print("sha512:\n" + str(hash.sha512(args[0], args[1])))
+    print("sha512/224:\n" + str(hash.sha512_t(args[0], 224)))
+    print("sha512/256:\n" + str(hash.sha512_t(args[0], 256)))
 # Call the main function after every potential function is defined
 main()
