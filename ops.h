8b04f5dedc139d949dab65d24498c7069c6422d4
b3nj5m1n <47924309+b3nj5m1n@users.noreply.github.com>
2019-07-13 15:02:44 +0200

    Added SIGMA0()

--- ops.h
+++ ops.h
@@ -12,3 +12,8 @@ uint64_t ROTR64(uint64_t x, int n)
 {
     return x >> n | x << 64-n;
 }
+
+// Sigma function as specified in FIPS-180-4 4.1.2
+uint32_t SIGMA0_32(uint32_t x) {
+    return ROTR32(x, 2) ^ ROTR32(x, 13) ^ ROTR32(x, 22);
+}
--- test.c
+++ test.c
@@ -4,9 +4,10 @@
 #include"ops.h"
 
 
+
 int main (int argc, char*argv[]) {
 
-    printf("%ld", ROTR64(5, 2));
+    printf("%d", SIGMA0_32(8));
 
     return 0;
 }
