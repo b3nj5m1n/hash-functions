28d683a4c9be389e3d7b1f678ab83388c4d2d3da
b3nj5m1n <47924309+b3nj5m1n@users.noreply.github.com>
2019-07-13 18:05:10 +0200

    Added MIX()
    But I don't know if it's working yet

--- sched.h
+++ sched.h
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<stdint.h>
+#include <math.h>
 
 // Rotate bits of a 32 bit integer to the right by n bits
 uint32_t SCHED_ROTR32(uint32_t x, int n)
@@ -15,4 +16,11 @@ uint32_t SCHED_SIGMA0_32(uint32_t x) {
 // Sigma 1 function as specified in FIPS-180-4 4.1.2
 uint32_t SCHED_SIGMA1_32(uint32_t x) {
     return SCHED_ROTR32(x, 17) ^ SCHED_ROTR32(x, 19) ^ (x >> 10);
+}
+
+uint32_t MIX(int t, int init_words[]) {
+    if (t >= 16) {
+        return (SCHED_SIGMA1_32(init_words[t-2]) + init_words[t-7]+ SCHED_SIGMA0_32(init_words[t-15]) + init_words[t-16]) % (int) pow(2, 32);
+    }
+    return init_words[t];
 }
